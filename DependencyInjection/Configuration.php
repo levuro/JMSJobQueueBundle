5c1957db2bde919a18561dad384bc4cb4145391f
Giuseppe Carcaci <gcar@levuro.com>
2023-09-15 14:24:12 +0200

    Fix Symfony\Component\Config\Definition\Builder\TreeBuilder::root() method called for the jms_job_queue configuration is deprecated since Symfony 4.3, pass the root name to the constructor instead.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -35,7 +35,13 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('jms_job_queue');
-        $rootNode = $treeBuilder->getRootNode();
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('jms_job_queue');
+        }
 
         $rootNode
             ->children()
